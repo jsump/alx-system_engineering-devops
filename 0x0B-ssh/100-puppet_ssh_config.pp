523b7781bdbc38a68b68252fe1c12bc1a14e1203
jsump <libbyn79@gmail.com>
2023-11-29 12:01:42 -0800

    Task 4 edit

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,8 +3,8 @@ file { '/home/ubuntu/.ssh/config':
   ensure  => file,
   content => "
     Host 100.25.31.195
-       IdentityFile /home/ubuntu/.ssh/school
+       IdentityFile ~/.ssh/school
        PasswordAuthentication no
   ",
-  mode  => '0600',
+  mode    => '0600',
 }
