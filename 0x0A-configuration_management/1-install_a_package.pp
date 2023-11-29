710350997270d182c6b6937e3e62a01ded9aeee0
jsump <libbyn79@gmail.com>
2023-11-29 03:31:23 -0800

    Task 1 edit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # This manifest is used to install a package
 package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
 }
