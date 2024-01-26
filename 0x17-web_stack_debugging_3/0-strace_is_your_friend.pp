05d2789f26aed568c90a37ebcf275f3be7eb4906
jsump <libyn79@gmail.com>
2024-01-26 21:38:09 +0000

    Edit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,5 @@
 # find out why apche is returning 500 erroe and fix it
 exec { 'affected_file':
-  command     => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path        => ['/usr/local/bin', '/usr/bin', '/bin'],
-  refreshonly => true,
-}
-
-service { 'apache2':
-  ensure  => 'running',
-  enable  => true,
-  require => Exec['affected_file'],
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/usr/bin/:/bin/',
 }
