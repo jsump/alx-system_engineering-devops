f545322e2fa5317fe319f3c293991c8e9313a86f
jsump <libbyn79@gmail.com>
2024-01-26 12:33:48 +0000

    Limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,21 @@
+# Optimize Nginx web server configuration
+
+# Install nginx
+package { 'nginx':
+  ensure => 'installed',
+}
+
+# Congigure nginx
+file { '/etc/nginx/nginx.conf':
+  ensure   => file,
+  content  => template('nginx/nginx.conf.erb'),
+  require  => Package['nginx'],
+  notify   => Service['nginx'],
+}
+
+# Define Service for nginx
+service { 'nginx':
+  ensure   => 'running',
+  enable   => true,
+  require  => Package['nginx'],
+}
