afd5f68f154b0d04302f57f1d0e39e393870502f
jsump <libbyn79@gmail.com>
2024-01-26 17:47:46 +0000

    Editt

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,15 +1,2 @@
-# Optimize Nginx web server configuration
-
-exec { 'change_nginx_config':
-  command     => '/usr/bin/sed -i s/15/1000/ /etc/default/nginx',
-  path        => ['/usr/bin'],
-  refreshonly => true,
-  notify      => Service['nginx'],
-}
-
-# endure nginx is running
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => Exec['change_nginx_config'],
-}
+# login with htbn user 
+exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
